6119de33e8b1b5daaf1bd754860353bd3cae9361
Rakkasan <rrich8205@gmail.com>
2015-01-28 23:44:06 -0500

    First Commit

---
+++ Bootstrap_test/dist/js/custom_h.js
@@ -0,0 +1,26 @@
+
+
+
+$(document).ready(function(){
+  $("#drop_button1").click(function(){
+    $("#drop").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#drop_button2").click(function(){
+    $("#drop2").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#drop_button3").click(function(){
+    $("#drop3").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#drop_button4").click(function(){
+    $("#drop4").slideToggle("slow");
+  });
+});
